3e2efbee8bf7f0219432b2b289290a4be00193f6
Ubuntu <ubuntu@ip-172-31-30-86.ap-northeast-1.compute.internal>
2021-02-02 09:21:30 +0000

    add doc pages

---
+++ docs/en.search-data.min.d5727e18aa6fe0f972eae328721632f60fd8f2326e5035de86acc705149de847.js
@@ -0,0 +1,1 @@
+'use strict';(function(){const indexCfg={cache:true};indexCfg.doc={id:'id',field:['title','content'],store:['title','href','section'],};const index=FlexSearch.create('balance',indexCfg);window.bookSearchIndex=index;index.add({'id':0,'href':'/aboutme/docs/','title':"Technical Contents",'section':"Top page",'content':"Technical Contents #  "});})();
